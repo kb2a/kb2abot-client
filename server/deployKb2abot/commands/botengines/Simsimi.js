65601a4f89a4b13f7e808b4eda293814bbf2d2d6
Khoa Ko Mlem <visualpascalc@gmail.com>
2020-11-26 22:22:27 -0800

    Đổi api simsimi

--- server/deployKb2abot/commands/botengines/Simsimi.js
+++ server/deployKb2abot/commands/botengines/Simsimi.js
@@ -11,38 +11,19 @@ module.exports = (body, api, parent, mssg) => {
 		parent
 	);
 	axios({
-		url: `http://sim.cunnobi.xyz/api/?text=${encodeURI(body)}&format=TEXT`,
+		url: `https://simsimi.copcute.pw/api/sim.php?text=${encodeURI(body)}`,
 		method: "GET",
 		mode: "no-cors"
 	}).then(res => {
-		try {
-			const jsonP = JSON.parse(/{(.*?)}/.exec(res.data)[0]);
-			if (jsonP.code == "0") {
-				const replyMsg = jsonP.text;
-				api.sendMessage(replyMsg, mssg.threadID);
-				log(
-					{
-						text: `SIMSIMI: "${replyMsg}"`,
-						icon: "robot",
-						bg: "bg1"
-					},
-					parent
-				);
-			} else {
-				const replyMsg = "Hong hỉu bạn nói j á :<";
-				api.sendMessage(replyMsg, mssg.threadID);
-				log(
-					{
-						text: `SIMSIMI: "${replyMsg}"`,
-						icon: "robot",
-						bg: "bg1"
-					},
-					parent
-				);
-			}
-		}
-		catch(e) {
-			console.log("Loi simsimi!");
-		}
+		const replyMsg = res.data.messages[0].text;
+		api.sendMessage(replyMsg, mssg.threadID);
+		log(
+			{
+				text: `SIMSIMI: "${replyMsg}"`,
+				icon: "robot",
+				bg: "bg1"
+			},
+			parent
+		);
 	});
 };
