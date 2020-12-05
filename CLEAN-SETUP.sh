05a946c7bf869943f9fff2cb8f46e2211320274f
Khoa Ko Mlem <khoakomlem@gmail.com>
2020-12-04 23:41:08 -0800

    Update CLEAN-SETUP.sh

--- CLEAN-SETUP.sh
+++ CLEAN-SETUP.sh
@@ -6,6 +6,8 @@ echo "Dang xoa thu muc .git . . ."
 rm -rf .git
 echo "Dang xoa thu muc node_modules . . ."
 rm -rf node_modules
+echo "Dang cai dat lai node_modules . . ."
+npm install
 echo "Da xong!"
 echo "Vui long chay lai bot lan dau!"
 read -p "Nhan phim bat ki de thoat . . ."
