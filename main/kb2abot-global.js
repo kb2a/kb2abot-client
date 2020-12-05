f5246def8d109bb065c36b489b6a9ef97c37ce6e
Khoa Ko Mlem <khoakomlem@gmail.com>
2020-12-04 23:42:38 -0800

    Update kb2abot-global.js

--- main/kb2abot-global.js
+++ main/kb2abot-global.js
@@ -1,17 +1,17 @@
 const form = {
-	id: 0,				// id của người dùng facebook
-	utils: {},			// ultilities
-	plugins: {}, 		// các plugins trong ./plugins
-	helpers: {}, 		// các helpers trong ./helpers
-	datastore: {},		// datastore (sử dụng nedb)
-	pluginStorage: {},	// lưu trữ các data của plugin (ví dụ engineName của plugin autoReply)
+	id: 0, // id của người dùng facebook
+	utils: {}, // các ultilities trong ./utils
+	plugins: {}, // các plugins trong ./plugins
+	helpers: {}, // các helpers trong ./helpers
+	datastore: {}, // datastore (sử dụng nedb)
+	pluginStorage: {} // lưu trữ các data của plugin (ví dụ engineName của plugin autoReply)
 };
 
 const apply = data => {
 	return Object.assign(form, data);
-}
+};
 
 module.exports = {
 	form,
-	apply,
+	apply
 };
