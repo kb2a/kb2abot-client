a99fb22d34224ebb34223ae14882afeec374d686
KhoaKoMlem <khoakomlem@gmail.com>
2020-12-20 01:44:37 -0800

    [update-plugin] autoreply

--- main/plugins/AutoReply.js
+++ main/plugins/AutoReply.js
@@ -1,34 +1,38 @@
 const os = require("os");
-const BotEngines = require("./botengines");
+const {botengines, fixEngineName} = kb2abot.utils.autoreply;
+const {getKeyword, getParam} = kb2abot.utils;
+const keywords = ["autoreply", "auto"];
 
 module.exports = {
 	type: "continuous",
 	friendlyName: "Tự động trả lời tin nhắn",
-	keywords: ["autoreply", "auto"],
-	description:
-		"Dùng để bật chức năng tự động trả lời cho bot, default là xài engine 'simsimi'",
-	extendedDescription: `/autoreply <engineName>${os.EOL}Các engineName: simsimi, mitsuku.`,
-	// preload: async api => {},
+	keywords,
+	description: "Dùng để bật chức năng tự động trả lời tin nhắn cho bot",
+	extendedDescription: `/autoreply <engineName>${os.EOL}Hiện tại gồm các engine: Simsimi, Mitsuku.${os.EOL}Để tắt bot thì dùng lệnh: /autoreply off`,
 	fn: async function(api, message) {
-		if (this.group.chat == false) {
+		if (getParam(message.body) == "off") {
 			api.sendMessage(
-				"Bạn chưa bật chat cho group, vui lòng sử dụng lệnh: /switch --chat on",
+				`${this.groupStorage.engine} chào tạm biệt ~~`,
 				message.threadID
 			);
-		} else {
-			const fixedEngineName = kb2abot.utils.autoreply.fixEngineName(message.body);
-			if (BotEngines[fixedEngineName]) {
-				this.engine = fixedEngineName;
-				api.sendMessage(
-					`Đã chuyển xài engine: ${fixedEngineName}`,
-					message.threadID
-				);
+			delete this.groupStorage.engine;
+			return;
+		}
+
+		if (keywords.includes(getKeyword(message.body))) {
+			const fixedEngineName = fixEngineName(message.body.split(" ")[1]);
+			if (botengines[fixedEngineName]) {
+				this.groupStorage.engine = fixedEngineName;
+				api.sendMessage(`${fixedEngineName} xin chào bạn!`, message.threadID);
 			} else {
 				api.sendMessage(
 					`Không tìm thấy engine nào có tên: ${fixedEngineName}`,
 					message.threadID
 				);
 			}
+		} else {
+			this.groupStorage.engine &&
+				botengines[this.groupStorage.engine](api, message);
 		}
 	}
 };
--- main/plugins/botengines/index.js
+++
@@ -1,4 +0,0 @@
-const Mitsuku = require("./Mitsuku.js");
-const Simsimi = require("./Simsimi.js");
-
-module.exports = {Mitsuku, Simsimi};
