46c58a6f45c87599f5c203051cc444878eaf5b81
Khoa Ko Mlem <khoakomlem@gmail.com>
2021-01-19 08:46:27 -0800

    rename org

--- main/bootloader/update.js
+++ main/bootloader/update.js
@@ -6,7 +6,7 @@ module.exports = {
 	fn: async () => {
 		const initResult = await git.init();
 		if (!initResult.existing) {
-			await git.addRemote("origin", "https://github.com/kb2abot/kb2abot");
+			await git.addRemote("origin", "https://github.com/kb2ateam/kb2abot");
 		}
 
 		await git.fetch("origin", "main"); //git fetch origin main
