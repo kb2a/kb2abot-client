516dbeeca44a9a82536f6a2d210643a6344ba4d3
khoakomlem <khoakomlem@gmail.com>
2021-01-28 01:53:15 +0700

    add htmldecode to mitsuku engine

--- main/utils/botengines/Mitsuku.js
+++ main/utils/botengines/Mitsuku.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const htmldecode = require("decode-html");
 
 // const MITSUKU_THREADID = "47719737069";
 // const TRANS_KEY = "trnsl.1.1.20200421T120324Z.a5209d29ad08ae85.7328dad8e46f00dad4a4cdb9a911868d8ace112b"; // you can use your own yandex translate API KEY
@@ -15,7 +16,7 @@ module.exports = (api, message) => {
 	}).then(res => {
 		const regexString = /<that>(.*?)<\/that>/.exec(res.data);
 		const responseChat = regexString ? regexString[1] : "...";
-		const replyMsg = responseChat.replace(/&quot;/g, "\"");
+		const replyMsg = htmldecode(responseChat).replace(/<br>/g, "\n");
 		// translate(replyMsg, {
 		// 	from: "en",
 		// 	to: "vi",
