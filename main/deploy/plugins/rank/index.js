7c595fed78464d466414f2df94adda5c2a37d097
Khoa Ko Mlem <khoakomlem@gmail.com>
2021-03-07 08:16:32 +0700

    move plugin rank

--- main/deploy/plugins/rank/index.js
+++ main/deploy/plugins/rank/index.js
@@ -37,57 +37,57 @@ const Diamond = {
 	"VI": "kim cương VI (hạng tối đa)"
 };
 
-function rankUp (xp, threadID, senderID, userStorage) {;
+function rankUp (xp, threadID, senderID, userStorage) {
 	const lmao = threadID + "uid_" + senderID;
-	
+
 	if (xp >= 10 && xp < 30) {
-		userStorage[lmao] = copper.I
+		userStorage[lmao] = copper.I;
 	}else if (xp >= 30 && xp < 50) {
-		userStorage[lmao] = copper.II
+		userStorage[lmao] = copper.II;
 	}else if (xp >= 50 && xp < 70) {
-		userStorage[lmao] = copper.III
+		userStorage[lmao] = copper.III;
 	}else if (xp >= 70 && xp < 90) {
-		userStorage[lmao] = silver.I
+		userStorage[lmao] = silver.I;
 	}else if (xp >= 90 && xp < 110) {
-		userStorage[lmao] = silver.II
+		userStorage[lmao] = silver.II;
 	}else if (xp >= 110 && xp < 130) {
-		userStorage[lmao] = silver.III
+		userStorage[lmao] = silver.III;
 	}else if (xp >= 130 && xp < 150) {
-		userStorage[lmao] = silver.IV
+		userStorage[lmao] = silver.IV;
 	}else if (xp >= 150 && xp < 170) {
-		userStorage[lmao] = gold.I
+		userStorage[lmao] = gold.I;
 	}else if (xp >= 170 && xp < 190) {
-		userStorage[lmao] = gold.II
+		userStorage[lmao] = gold.II;
 	}else if (xp >= 190 && xp < 210) {
-		userStorage[lmao] = gold.III
+		userStorage[lmao] = gold.III;
 	}else if (xp >= 210 && xp < 230) {
-		userStorage[lmao] = gold.IV
+		userStorage[lmao] = gold.IV;
 	}else if (xp >= 230 && xp < 250) {
-		userStorage[lmao] = platinum.I
+		userStorage[lmao] = platinum.I;
 	}else if (xp >= 250 && xp < 270) {
-		userStorage[lmao] = platinum.II
+		userStorage[lmao] = platinum.II;
 	}else if (xp >= 270 && xp < 290) {
-		userStorage[lmao] = platinum.III
+		userStorage[lmao] = platinum.III;
 	}else if (xp >= 290 && xp < 310) {
-		userStorage[lmao] = platinum.IV
+		userStorage[lmao] = platinum.IV;
 	}else if (xp >= 310 && xp < 330) {
-		userStorage[lmao] = platinum.V
+		userStorage[lmao] = platinum.V;
 	}else if (xp >= 330 && xp < 350) {
-		userStorage[lmao] = Diamond.I
+		userStorage[lmao] = Diamond.I;
 	}else if (xp >= 350 && xp < 370) {
-		userStorage[lmao] = Diamond.II
+		userStorage[lmao] = Diamond.II;
 	}else if (xp >= 370 && xp < 390) {
-		userStorage[lmao] = Diamond.III
+		userStorage[lmao] = Diamond.III;
 	}else if (xp >= 390 && xp < 410) {
-		userStorage[lmao] = Diamond.IV
+		userStorage[lmao] = Diamond.IV;
 	}else if (xp >= 410 && xp < 430) {
-		userStorage[lmao] = Diamond.V
+		userStorage[lmao] = Diamond.V;
 	}else if (xp >= 430) {
-		userStorage[lmao] = Diamond.VI
+		userStorage[lmao] = Diamond.VI;
 	}else {
-		userStorage[lmao] = "chưa có"
+		userStorage[lmao] = "chưa có";
 	}
-};
+}
 	
 module.exports = {
 	authorDetails: {
@@ -121,7 +121,7 @@ module.exports = {
 	// true là tắt, false là bật
 
 	onLoad: async function() {
-		console.log("plugin rank v0.4 (beta)")
+		console.log("plugin rank v0.4 (beta)");
 		// Được gọi ngay sau khi load xong plugin
 		// Chủ yếu dùng để log thôi không quan trọng
 		// (Hoặc cũng có thể chuẩn bị các async function bằng await)
@@ -129,19 +129,19 @@ module.exports = {
 
 	onMessage: async function(api, message) {
 		if (!this.storage.account.global.messageCount) {
-			this.storage.account.global.messageCount = {}
-		};
+			this.storage.account.global.messageCount = {};
+		}
 		if (!this.storage.account.global.rank) {
-			this.storage.account.global.rank = {}
-		};
-		
+			this.storage.account.global.rank = {};
+		}
+
 		if (this.storage.account.global.messageCount[message.threadID + "uid_" + message.senderID] == null) {
-			this.storage.account.global.messageCount[message.threadID + "uid_" + message.senderID] = 0
+			this.storage.account.global.messageCount[message.threadID + "uid_" + message.senderID] = 0;
 		}else {
-			this.storage.account.global.messageCount[message.threadID + "uid_" + message.senderID]++
-		};
+			this.storage.account.global.messageCount[message.threadID + "uid_" + message.senderID]++;
+		}
 		if (this.storage.account.global.rank[message.threadID + "uid_" + message.senderID] == null) {
-			this.storage.account.global.rank[message.threadID + "uid_" + message.senderID] = "chưa có"
+			this.storage.account.global.rank[message.threadID + "uid_" + message.senderID] = "chưa có";
 		}else {
 			rankUp(this.storage.account.global.messageCount[message.threadID + "uid_" + message.senderID], message.threadID, message.senderID, this.storage.account.global.rank);
 		}
@@ -150,21 +150,21 @@ module.exports = {
 		// Xử lí mọi tin nhắn mà không cần prefix
 	},
 
-	onCall: async function(api, message) {		
+	onCall: async function(api, message) {
 		function reply(msg) {
-			api.sendMessage(msg, message.threadID, message.messageID)
-		};
-		
+			api.sendMessage(msg, message.threadID, message.messageID);
+		}
+
 		const EXP = this.storage.account.global.messageCount[message.threadID + "uid_" + message.senderID];
 		const getRank = this.storage.account.global.rank[message.threadID + "uid_" + message.senderID];
-		
+
 		api.getUserInfo(message.senderID, (err, ret) => {
 			for (let prop in ret) {
 				if (ret.hasOwnProperty(prop) && ret[prop].name) {
-					reply(`${ret[prop].name}(uid: ${message.senderID})\r\n---\r\nlevel: ${round(EXP/20, 0)}\r\nxp: ${EXP}\r\n---\r\nrank: ${getRank}`)
+					reply(`${ret[prop].name}(uid: ${message.senderID})\r\n---\r\nlevel: ${round(EXP/20, 0)}\r\nxp: ${EXP}\r\n---\r\nrank: ${getRank}`);
 				}
 			}
-		})
+		});
 		// Được gọi khi có member xài lệnh của mình
 		// Là cốt lõi của plugin không có phần này thì có nghĩa sẽ không có chuyện
 		// gì xảy ra khi gọi plugin (để hideFromHelp true nữa là plugin như batman)
