f1213ed93294adbba3e799b5a09a60099f93f307
Đậu Văn Đăng Khoa <khoakomlem@gmail.com>
2021-11-01 16:54:38 +0700

    update simsimi api to v2

--- main/deploy/plugins/internal/autoreply/botengines/Simsimi.js
+++ main/deploy/plugins/internal/autoreply/botengines/Simsimi.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 module.exports = async (message, reply) => {
 	const res = await axios({
-		url: `https://api.simsimi.net/v1/?text=${encodeURI(message.body)}`,
+		url: `https://api.simsimi.net/v2/?text=${encodeURI(message.body)}`,
 		method: 'GET'
 	});
 	reply(res.data.success);
